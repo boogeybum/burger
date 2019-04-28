acd3ef106f9ddb3426de5eab6845ed8cf8da3000
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-28 16:44:19 -0500

    Created the controllers  folder with burgers_controller.js file inside. Set dependency requirements and router variable to export.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+// import required dependencies
+var express = require("express");
+var burger = require("../models/burger");
+
+// router to control how this app functions
+var router;
+
+// export router
+module.exports = router;
