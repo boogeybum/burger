2097ecc97014f923d3002d48550d7f7e787d468a
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-28 15:41:00 -0500

    Basic shell code for burger that contains 3 functions for interatcing with the database

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,22 @@
 var orm = require("../config/orm");
 
 // Call the ORM for burger specific input for the ORM
+var burger = {
+    // select all entires in table
+    selectAll: function(callBack) {
+
+    },
+
+    // input into one of the entries
+    insertOne: function(callBack) {
+        
+    },
+
+    // update one of the table entries
+    updateOne: function(callBack) {
+
+    }
+};
+
+// export the function for use in the controller
+module.exports = burger;
