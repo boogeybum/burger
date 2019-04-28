79eaf9a1518907cea4b647ab7a6c0a6b01ae361d
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-28 17:02:45 -0500

    Set routing for linking through express router to the database

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,37 @@
-// import required dependencies
+// import required express and set router
 var express = require("express");
+var router = express.Router();
+
+// require burger.js to use database functions
 var burger = require("../models/burger");
 
-// router to control how this app functions
-var router;
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var burgerObject = {
+            burgers: data
+        };
+        res.render("index", burgerObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.insertOne([
+        "burger_name",
+    ], [
+        req.body.burger_name
+    ], function(data) {
+        res.redirect("/");
+    });
+});
+
+router.post("/", function(req, res) {
+    var burgerState = "id = " + req.params.id;
+    burger.updateOne({
+        devoured: true
+    }, burgerState, function(data) {
+        res.redirect("/");
+    });
+});
 
 // export router
 module.exports = router;
