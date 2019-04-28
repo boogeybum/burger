cfefcad6ef9d0040bc0e3aba5e55f339b8f78a45
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-27 23:01:04 -0500

    pseudo coded the ORM file and made ready for export

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,11 @@
 // import mysql connection
 var connection = require("../config/connection");
 
+// select all the data from burgers_db
+
+// insert one entry into the burgers_db
+
+// update one record in the burgers_db
+
+// export the orm object
+module.exports = orm;
