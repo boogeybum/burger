84d56d88e3407dd995a83d1ba5b77e32f90dc605
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-28 19:26:22 -0500

    Updated server.js with dependencies and port process. Added hamburguer.png to img file and display on index.handlebars.

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 var express = require("express");
 
+var routes = require("./controllers/burgers_controller");
+
+var PORT = process.env.PORT || 8080;
+
+// set up app
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Eat-Da-Burger App is listening on PORT " + PORT);
+});
